a24f2a3b865206ee9066e9287293ae419523925a
slim144 <82081180+slim144@users.noreply.github.com>
2023-01-30 19:12:42 +0800

    added redux persist for cart and added redux devtool for development environment

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,22 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import logger from "redux-logger";
 
 import { rootReducer } from "./root-reducer";
 
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["cart"],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const store = configureStore({
-  reducer: rootReducer,
-  middleware: [logger],
+  reducer: persistedReducer,
+  middleware: [process.env.NODE_ENV !== "production" && logger].filter(Boolean),
+  devTools: process.env.NODE_ENV !== "production",
 });
+
+export const persistor = persistStore(store);
